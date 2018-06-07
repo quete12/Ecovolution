b38372ddea920d60bf19c50b8ce14b4d75ea8c73
quete12 <quentin.schaer@gmx.ch>
2018-06-07 13:22:15 +0200

    Implement Tiles

--- src/main/java/ch/schaermedia/ecovolution/environment/Tile.java
+++ src/main/java/ch/schaermedia/ecovolution/environment/Tile.java
@@ -5,16 +5,42 @@
  */
 package ch.schaermedia.ecovolution.environment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Quentin
  */
 public class Tile {
+
     private static final int NUMBER_OF_ATMOSPHERELAYERS = 3;
 
     private CompoundMix[] layers;
 
     public Tile() {
         this.layers = new CompoundMix[NUMBER_OF_ATMOSPHERELAYERS];
+    }
+
+    public void calculate(List<Tile> tiles) {
+        for (int i = 0; i < layers.length; i++) {
+            List<CompoundMix> neighbours = new ArrayList<>();
+            for (Tile tile : tiles) {
+                neighbours.add(tile.getMixAtLayer(i));
+            }
+            CompoundMix higher = i < layers.length - 1 ? layers[i + 1] : null;
+            CompoundMix lower = i > 0 ? layers[i - 1] : null;
+            layers[i].spread(neighbours, higher, lower);
+        }
+    }
+
+    public void update() {
+        for (CompoundMix layer : layers) {
+            layer.update();
+        }
+    }
+
+    public CompoundMix getMixAtLayer(int layer) {
+        return layers[layer];
     }
 }
